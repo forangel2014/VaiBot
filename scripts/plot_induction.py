5b1f4c0bac2e4c2f464f4bd15320c1feb497b8d1
forangel2014 <sunwangtao2021@ia.ac.cn>
2024-12-18 08:15:59 +0000

    update

--- scripts/plot_induction.py
+++ scripts/plot_induction.py
@@ -2,7 +2,7 @@ import os
 import matplotlib.pyplot as plt
 
 exp_dir = "../exp_induction"
-selected_epoch = 1
+selected_epoch = 10
 
 nesy_observed_samples = []
 nesy_seen_induction_accuracy = []
@@ -49,18 +49,18 @@ for exp_name in os.listdir(exp_dir):
         find_seen_induction_accuracy = False
         find_unseen_induction_accuracy = False
         if os.path.isdir(exp_path):
-            induction_path = os.path.join(exp_path, f"epoch{selected_epoch}", "neural2symbolic.log")
+            induction_path = os.path.join(exp_path, "symbolic_task.log")
             try:
                 with open(induction_path, "r") as f:
                     # 分别找到seen task accuracy和unseen task accuracy
                     lines = f.readlines()
                     for line in lines:
-                        if "accuracy on seen task" in line:
+                        if "symbolic seen task accuracy" in line:
                             find_seen_induction_accuracy = True
-                            this_seen_induction_accuracy = float(line.split("samples: ")[1].split(" ")[0])
-                        elif "accuracy on unseen task" in line:
+                            this_seen_induction_accuracy = float(line.split("finetuning: ")[1].split(" ")[0])
+                        elif "symbolic unseen task accuracy" in line:
                             find_unseen_induction_accuracy = True
-                            this_unseen_induction_accuracy = float(line.split("samples: ")[1].split(" ")[0])
+                            this_unseen_induction_accuracy = float(line.split("finetuning: ")[1].split(" ")[0])
 
             except Exception as e:
                 print(f"Error reading file {induction_path}: {e}")
@@ -72,18 +72,18 @@ for exp_name in os.listdir(exp_dir):
                 sft_unseen_induction_accuracy.append(this_unseen_induction_accuracy)
 
 # 将所有列表按pretrain_ratios升序排序
-nesy_observed_samples, nesy_seen_induction_accuracy, nesy_unseen_induction_accuracy \
-= zip(*sorted(zip(nesy_observed_samples, nesy_seen_induction_accuracy, nesy_unseen_induction_accuracy), key=lambda x: x[0]))
+if len(nesy_observed_samples) > 0:
+    nesy_observed_samples, nesy_seen_induction_accuracy, nesy_unseen_induction_accuracy \
+    = zip(*sorted(zip(nesy_observed_samples, nesy_seen_induction_accuracy, nesy_unseen_induction_accuracy), key=lambda x: x[0]))
+    nesy_induction_accuracy = [0.9 * seen + 0.1 * unseen for seen, unseen in zip(nesy_seen_induction_accuracy, nesy_unseen_induction_accuracy)]
+    plt.plot(nesy_observed_samples, nesy_induction_accuracy, label="nesy", color='#2ecc71', marker='o')
 
-sft_observed_samples, sft_seen_induction_accuracy, sft_unseen_induction_accuracy \
-= zip(*sorted(zip(sft_observed_samples, sft_seen_induction_accuracy, sft_unseen_induction_accuracy), key=lambda x: x[0]))
 
-# 将seen和unseen的accuracy按0.9和0.1的比例混合，得到induction的accuracy
-nesy_induction_accuracy = [0.9 * seen + 0.1 * unseen for seen, unseen in zip(nesy_seen_induction_accuracy, nesy_unseen_induction_accuracy)]
-sft_induction_accuracy = [0.9 * seen + 0.1 * unseen for seen, unseen in zip(sft_seen_induction_accuracy, sft_unseen_induction_accuracy)]
-
-plt.plot(nesy_observed_samples, nesy_induction_accuracy, label="nesy", color='#2ecc71', marker='o')
-plt.plot(sft_observed_samples, sft_induction_accuracy, label="sft", color='#3498db', marker='o')
+if len(sft_observed_samples) > 0:
+    sft_observed_samples, sft_seen_induction_accuracy, sft_unseen_induction_accuracy \
+    = zip(*sorted(zip(sft_observed_samples, sft_seen_induction_accuracy, sft_unseen_induction_accuracy), key=lambda x: x[0]))
+    sft_induction_accuracy = [0.9 * seen + 0.1 * unseen for seen, unseen in zip(sft_seen_induction_accuracy, sft_unseen_induction_accuracy)]
+    plt.plot(sft_observed_samples, sft_induction_accuracy, label="sft", color='#3498db', marker='o')
 
 #横轴标签：pretrain ratio
 plt.xlabel("observed samples")
